6842f6733b8892b96f431c78d7bb88f676d5a530
Lucasdelpiero <lucasdelpiero98@gmail.com>
2025-08-28 20:34:19 -0300

    3-Agregado otra opcion

--- guia-2/3-variables-enteras-dinamicas/main.c
+++ guia-2/3-variables-enteras-dinamicas/main.c
@@ -4,13 +4,15 @@
 
 void creaVars(int *ptr, int n);
 void escribeVars(int *ptr, int n);
+int* creaVarsPtr(int n);
 
 int main()
 {
     int *ptr;
-    ptr = (int *) malloc(sizeof(int) * CANT);
-    creaVars(ptr, CANT);
-    escribeVars(ptr, CANT);
+    //ptr = (int *) malloc(sizeof(int) * CANT); // Opcion 1 asigna memoria en main
+    //creaVars(ptr, CANT);                      // Opcion 1
+    ptr = creaVarsPtr(CANT);                    // Opcion 2 devuelve puntero que apunta al lugar de memoria asignado en funcion
+    escribeVars(ptr, CANT);                     // Preguntar cual es mejor y cual (creo que la 2)
     free(ptr);
 
     return 0;
@@ -25,6 +27,20 @@ void creaVars(int *ptr, int n){
         scanf("%d", ptrTemp);
         ptrTemp++;
     }
+}
+
+int* creaVarsPtr(int n){
+    unsigned int i;
+    int *ptr = malloc(sizeof(int)*n);
+    int *ptrTemp = ptr;
+
+    for(i=0; i < n; i++){
+        printf("Ingrese el valor del valor n:%d\n", i+1);
+        scanf("%d", ptrTemp);
+        ptrTemp++;
+    }
+
+    return ptr;
 }
 
 void escribeVars(int *ptr, int n){
