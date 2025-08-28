06405e9eae57b23d9456d3998a5be6eaf5da975a
Lucasdelpiero <lucasdelpiero98@gmail.com>
2025-08-28 19:44:08 -0300

    Guia 2 - 1 y 2

---
+++ guia-2/2-ordenados/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a = 30, b = 20;
+    ordenadas(&a, &b);
+    printf(" valor de a %d\tvalor de b %d\n", a, b);
+    return 0;
+}
+
+void ordenadas(int *x, int *y){
+    int* aux;
+    if(x > y) {
+        aux = *x;
+        *x = *y;
+        *y = aux;
+    }
+}
