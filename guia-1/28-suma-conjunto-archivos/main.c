c090bb93cb3327d69ce5f1c87cec251e54b5c413
Lucasdelpiero <lucasdelpiero98@gmail.com>
2025-08-30 17:19:12 -0300

    28

---
+++ guia-1/28-suma-conjunto-archivos/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    unsigned int total=0, n;
+    FILE *arch;
+
+    arch = fopen("datos.txt", "rt");
+    fscanf(arch, "%d", &n);
+    while(n != 0){
+        total += n;
+        printf("%d leido del archivo\n", n );
+        fscanf(arch, "%d", &n);
+    }
+    printf("El resultado es: %u\n", total);
+
+    return 0;
+}
