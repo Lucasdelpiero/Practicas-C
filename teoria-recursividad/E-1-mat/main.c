a8d8fd4736bfb6829da2413e7fdce89843fdc2e9
Lucasdelpiero <lucasdelpiero98@gmail.com>
2025-09-08 09:23:55 -0300

    Correccion

--- teoria-recursividad/E-1-mat/main.c
+++ teoria-recursividad/E-1-mat/main.c
@@ -10,18 +10,18 @@ int main()
     int n,m;
     n = 10; m = 2;
     int A[MAX][10] = {{1,2},{3,4},{5,6},{7,8},{9,10},{11,12},{13,14},{15,16},{22,13},{17,18}};
-    printf ("suma de los elementos %d", sumaM(A,0,0,n-1, m-1));
+    printf ("suma de los elementos %d", sumaM(A,n-1,m-1,n-1, m-1));
     return 0;
 }
 
 
 int sumaM(int A[][10], int i, int j, int n, int m){
-    if (i==n && j==m)
+    if (i==0 && j==0)
         return A[i][j];
     else
-        if (i==n)
-            return A[i][j]+sumaM(A,0,j+1,n,m);
+        if (i==0)
+            return A[i][j]+sumaM(A,n,j-1,n,m);
         else
-            return A[i][j]+sumaM(A,i+1,j,n,m);
+            return A[i][j]+sumaM(A,i-1,j,n,m);
 }
 
