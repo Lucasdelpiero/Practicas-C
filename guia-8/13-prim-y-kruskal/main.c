b4aea13efb0fd60aebe9db5dda28e4a4cdcccd27
Lucasdelpiero <lucasdelpiero98@gmail.com>
2025-11-04 14:25:57 -0300

    g8e13 prim y kuskal finalizado

--- guia-8/13-prim-y-kruskal/main.c
+++ guia-8/13-prim-y-kruskal/main.c
@@ -5,34 +5,40 @@
 #define MAX 20
 
 void cargaMatriz(int mat[MAX][MAX], int n);
-void prim(int mat[MAX][MAX], int arbolMin[MAX][MAX],int n); // a
-void kruskal(int mat[MAX][MAX], int arbolMin[MAX][MAX],int n); // a
+void prim(int mat[MAX][MAX], int arbolMin[MAX][MAX],int n, int ini); // a
+void kruskal(int mat[MAX][MAX], int arbolMin[MAX][MAX],int n); // b
+void buscaMin(int mat[MAX][MAX], int n, int cc[MAX], int * min, int *iMin, int *jMin);
 
 int main()
 {
     int grafoPond[MAX][MAX] = {0};
-    int arbolMin[MAX][MAX] = {0};
-    int n = 6;
+    int arbolMinPRIM[MAX][MAX] = {0}, arbolMinKruskal[MAX][MAX] = {0};;
+    int n = 6, ini;
     cargaMatriz(grafoPond, n);
+    do{
+        printf("Ingrese valor desde donde quiere comenzar prim (1-6)\n");
+        scanf("%d", &ini);
+    } while (ini < 1 || ini > 6);
     mostrarMatriz(grafoPond, n); printf("\n");
-    prim(grafoPond, arbolMin, n); printf("\n");
-    mostrarMatriz(arbolMin, n); printf("\n");
-    primMartin(grafoPond, n, 0, arbolMin);
-    mostrarMatriz(arbolMin, n);
+    prim(grafoPond, arbolMinPRIM, n, ini - 1); printf("\n");
+    //mostrarMatriz(arbolMinPRIM, n); printf("\n");
+
+    kruskal(grafoPond, arbolMinKruskal, n);
+    //mostrarMatriz(arbolMinKruskal, n);
+
+
+    //primMartin(grafoPond, n, 0, arbolMinPRIM);
+    //mostrarMatriz(arbolMinPRIM, n); printf("\n");
     return 0;
 }
 
-// b
-void kruskal(int mat[MAX][MAX], int arbolMin[MAX][MAX],int n) {
-
-}
 
 // a
-void prim(int mat[MAX][MAX], int arbolMin[MAX][MAX], int n){
+void prim(int mat[MAX][MAX], int arbolMin[MAX][MAX], int n, int ini){
     int u[MAX] = {0}, i, iMin, j, jMin, min;
     int disp = n-1;
 
-    int INI = 4; // Arranca desde el num 5 como en la teoria
+    int INI = ini; // Arranca desde el num 5 como en la teoria
     u[INI] = 1;
     printf("Arbol PRIM:\n");
     //printf("%d ", INI + 1);
@@ -51,11 +57,13 @@ void prim(int mat[MAX][MAX], int arbolMin[MAX][MAX], int n){
             }
         }
         u[jMin] = 1; // El destino mas barato se agrega a los visitados para el proximo ciclo
-        printf("(%d, %d) ", iMin + 1,jMin + 1);
+        printf("(%d, %d): %d ", iMin + 1,jMin + 1, min);
         //printf("%d ", jMin + 1);
         arbolMin[iMin][jMin] = min;
         arbolMin[jMin][iMin] = min;
         disp--;
+        if (disp > 0)
+            printf("--- ");
     }
     printf("\n");
 }
@@ -90,6 +98,46 @@ void primMartin(int mat[][MAX], int n, int ini, int arbolGen[][MAX]) { //verific
     }
 }
 
+// b
+void kruskal(int mat[MAX][MAX], int arbolMin[MAX][MAX],int n) {
+    int cc[MAX] = {0};
+    int i, j, k;
+    int min, iMin, jMin, aristas = 0, reemplazar;
+    for(i = 0; i < n; i++)
+        cc[i] = i + 1;
+
+    printf("Arbol Kruskal:\n");
+    while (aristas < n - 1){
+        buscaMin(mat, n, cc, &min, &iMin, &jMin);
+
+        // Reemplaza todos los que tengan un valor de otra cc con el valor de la cc al que se unieron
+        reemplazar = cc[jMin];
+        for(k = 0; k < n; k++)
+            if(cc[k] == reemplazar)
+                cc[k] = cc[iMin];
+
+        arbolMin[iMin][jMin] = min;
+        arbolMin[jMin][iMin] = min;
+        aristas++;
+        printf("(%d, %d): %d ", iMin + 1, jMin + 1, min);
+        if (aristas < n - 1)
+            printf("--- ");
+    }
+    printf("\n");
+}
+
+void buscaMin(int mat[MAX][MAX], int n, int cc[MAX], int * min, int *iMin, int *jMin){
+    int i, j;
+    *min = 999;
+    for(i=0; i<n; i++)
+        for(j=0; j<n; j++)
+            if (mat[i][j] != 0 && mat[i][j] < *min && cc[i] != cc[j]){ // cc[i] != cc[j] evita que se formen ciclos
+                    *min = mat[i][j];
+                    *iMin = i;
+                    *jMin = j;
+            }
+}
+
 // Esto para que???
 int esta(int vec[], int n, int x){
     int es = 0;
@@ -113,6 +161,8 @@ void cargaMatriz(int mat[MAX][MAX], int n){
     dat[2][3] = dat[3][2] = 5;
     dat[2][4] = dat[4][2] = 5;
     dat[2][5] = dat[5][2] = 4;
+
+    dat[3][5] = dat[5][3] = 2;
 
     dat[4][5] = dat[5][4] = 6;
 
